0858202654a66d9a5d10ca0444511f524fa54599
halourou <halourou@gmail.com>
2016-05-02 00:10:13 -0400

    CreateWorksapce table and User/Workspace join table

---
+++ db/migrate/20160502030257_users_workspaces.rb
@@ -0,0 +1,11 @@
+
+class UsersWorkspaces < ActiveRecord::Migration
+  def change
+    create_table :users_workspaces do |t|
+      t.integer :user_id
+      t.integer :workspace_id
+
+      t.timestamps
+    end
+  end
+end
