698e0c1327478264ea9c31af637d7783e492e673
halourou <halourou@gmail.com>
2016-05-04 17:04:38 -0400

    Changes flash errors to flash now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      sign_in!(@user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
-end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
